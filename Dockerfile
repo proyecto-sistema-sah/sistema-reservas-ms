e55fc847d0699aec0a2c5b7388ea99cb0bbbca40
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-12-03 18:28:33 -0500

    Se ajustan archivos

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,6 @@ WORKDIR /app
 # Copia el archivo JAR generado al contenedor
 COPY target/reservas-0.0.1-SNAPSHOT.jar reservas-ms.jar
 
-# Copia el directorio .aws al contenedor
-COPY .aws /root/.aws
-
 # Expone el puerto 8002 (o el puerto que usará tu aplicación)
 EXPOSE 8002
 
--- src/main/java/com/sistema/sah/reservas/configuration/S3Config.java
+++ src/main/java/com/sistema/sah/reservas/configuration/S3Config.java
@@ -3,7 +3,9 @@ package com.sistema.sah.reservas.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,14 +17,26 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         try {
-            Region region = new DefaultAwsRegionProviderChain().getRegion();
-            log.info("Detected AWS Region: {}", region);
+            // Leer variables de entorno configuradas en Azure
+            String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+            String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+            String regionName = System.getenv("AWS_REGION");
 
+            if (accessKey == null || secretKey == null || regionName == null) {
+                throw new IllegalStateException("AWS environment variables are not set");
+            }
+
+            // Configurar las credenciales de AWS
+            AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+            // Crear el cliente S3 con las credenciales y región especificadas
+            Region region = Region.of(regionName);
             S3Client client = S3Client.builder()
                     .region(region)
-                    .credentialsProvider(DefaultCredentialsProvider.create())
+                    .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                     .build();
-            log.info("S3 Client successfully created.");
+
+            log.info("S3 Client successfully created for region: {}", region);
             return client;
         } catch (Exception e) {
             log.error("Error creating S3 Client: ", e);
