25ae14b9f85905f34fabcf7996f86ebd40714f94
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-12-03 18:07:11 -0500

    Se ajusta despliegue

--- src/main/java/com/sistema/sah/reservas/configuration/S3Config.java
+++ src/main/java/com/sistema/sah/reservas/configuration/S3Config.java
@@ -1,21 +1,33 @@
 package com.sistema.sah.reservas.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@Slf4j
 public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-        // Configurar el cliente de S3 con DefaultCredentialsProvider y DefaultAwsRegionProviderChain
-        return S3Client.builder()
-                .region(new DefaultAwsRegionProviderChain().getRegion()) // Detectar región automáticamente
-                .credentialsProvider(DefaultCredentialsProvider.create()) // Detectar credenciales automáticamente
-                .build();
+        try {
+            Region region = new DefaultAwsRegionProviderChain().getRegion();
+            log.info("Detected AWS Region: {}", region);
+
+            S3Client client = S3Client.builder()
+                    .region(region)
+                    .credentialsProvider(DefaultCredentialsProvider.create())
+                    .build();
+            log.info("S3 Client successfully created.");
+            return client;
+        } catch (Exception e) {
+            log.error("Error creating S3 Client: ", e);
+            throw e;
+        }
     }
 
 }
