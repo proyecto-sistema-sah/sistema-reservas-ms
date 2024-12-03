7267e266a6743dd546f5be8c9e3ff70e0e3b8cc2
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-12-03 16:43:53 -0500

    Se ajusta archivo de despliegue

--- src/main/java/com/sistema/sah/reservas/configuration/S3Config.java
+++ src/main/java/com/sistema/sah/reservas/configuration/S3Config.java
@@ -1,9 +1,7 @@
 package com.sistema.sah.reservas.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
